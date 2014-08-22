595a32b0531ceefbbf4a25b5219c4a10c6e4dbd4
xiong.xiaox <xiong.xiaox@alibaba-inc.com>
2014-08-22 13:39:58 +0800

    Update gunicorn config file cpu numbers.

--- gunicorn.py
+++ gunicorn.py
@@ -1,7 +1,6 @@
-import multiprocessing
 import os
 
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 2
 bind = '0.0.0.0:8080'
 proc_name = 'website'
 pidfile = '%s/website.pid' % os.path.abspath(os.path.dirname(__file__))
