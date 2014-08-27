642de1c2bca57604da577a626afa623e7ff3752e
xiong.xiaox <xiong.xiaox@alibaba-inc.com>
2014-08-27 16:47:06 +0800

    Update cert build script and gunicorn config file for HTTPS.

--- gunicorn.py
+++ gunicorn.py
@@ -1,8 +1,12 @@
 import os
 
 workers = 2
-bind = '0.0.0.0:8080'
+bind = '0.0.0.0:443'
 proc_name = 'website'
 pidfile = '%s/website.pid' % os.path.abspath(os.path.dirname(__file__))
 accesslog = '%s/logs/gunicorn-access.log' % os.path.abspath(os.path.dirname(__file__))
 errorlog = '%s/logs/gunicorn-error.log' % os.path.abspath(os.path.dirname(__file__))
+
+ca_certs = '%s/instance/ca.crt' % os.path.abspath(os.path.dirname(__file__))
+certfile = '%s/instance/server.crt' % os.path.abspath(os.path.dirname(__file__))
+keyfile = '%s/instance/server.key' % os.path.abspath(os.path.dirname(__file__))
